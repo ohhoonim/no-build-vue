bd327c848e6b718847e7955a3b088331bcacc795
주형돈 <hdju@rntime.com>
2022-12-10 12:15:35 +0900

    condition,list,computed

---
+++ js/uuid-generator.js
@@ -0,0 +1,10 @@
+export default function* UUIDGenerator() {
+  let d, r;
+  while (true) {
+    yield 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+      r = (new Date().getTime() + Math.random() * 16) % 16 | 0;
+      d = Math.floor(d / 16);
+      return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+  }
+};
